153ab05618d60fb701e1fc284cb423d4ee8c837d
Marnel Gnacadja <marnel.gnacadja@paydunya.com>
2021-08-16 12:10:56 +0100

    Integreting the frontend

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,25 @@
 <x-layout>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+</x-layout>
+
+
+
+
+{{--
+<x-layout>
     @foreach($posts as $post)
         <article>
             <h2>
@@ -15,3 +36,4 @@
         </article>
     @endforeach
 </x-layout>
+--}}
