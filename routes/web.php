759b47a1952ab085ef4ab80d138ca5be77dfb970
Marnel Gnacadja <marnel.gnacadja@paydunya.com>
2021-08-17 11:03:13 +0100

    Extracting the dropdown component

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
         'posts' => Post::latest()->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 
 Route::get('/posts/{post:slug}', function (Post $post) {
